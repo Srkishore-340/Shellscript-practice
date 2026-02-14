69871fbafcfcbb824cba71ce3c9c354f089c5be4
RaviKishore Sara <ravikishoresara340@gmail.com>
2026-02-14 12:00:23 +0530

    added conversation code

---
+++ conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Bhaskar :: Bramham bro okati cheppali"
+echo "Brahmam :: Cheppu Bro"
+echo "Bhaskar :: MG "
+echo "Brahmam :: Enti brto antha mata annav" 
+echo "bhaskar :: hahhahaha "
